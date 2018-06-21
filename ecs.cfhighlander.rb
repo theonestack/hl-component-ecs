30bb6d1854df71a78eac7b44a3f9ed6cb507e16c
Nikola Tosic <tosicnikola10@gmail.com>
2018-06-21 19:38:03 +1000

    WIP cfhighlander syntax

---
+++ ecs.cfhighlander.rb
@@ -0,0 +1,38 @@
+CfhighlanderTemplate do
+  DependsOn 'vpc'
+  Parameters do
+    ComponentParam 'EnvironmentName', 'dev', isGlobal: true
+    ComponentParam 'EnvironmentType', 'development', isGlobal: true
+    ComponentParam 'Ami', type: 'AWS::EC2::Image::Id'
+    MappingParam('InstanceType') do
+      map 'EnvironmentType'
+      attribute 'EcsInstanceType'
+    end
+    MappingParam('AsgMin') do
+      map 'EnvironmentType'
+      attribute 'EcsAsgMin'
+    end
+    MappingParam('AsgMax') do
+      map 'EnvironmentType'
+      attribute 'EcsAsgMax'
+    end
+    MappingParam('KeyName') do
+      map 'AccountId'
+      attribute 'KeyName'
+    end
+    MappingParam('DnsDomain') do
+      map 'AccountId'
+      attribute 'DnsDomain'
+    end
+
+    maximum_availability_zones.times do |az|
+      ComponentParam "SubnetCompute#{az}"
+    end
+
+    ComponentParam 'VPCId', type: 'AWS::EC2::VPC::Id'
+    ComponentParam 'SecurityGroupLoadBalancer', type: 'AWS::EC2::SecurityGroup::Id'
+    ComponentParam 'SecurityGroupBastion', type: 'AWS::EC2::SecurityGroup::Id'
+    ComponentParam 'FileSystem' if enable_efs
+
+  end
+end
